804bee89735bfad4057683ab20739e701c326f52
ro0sterjam <khkwang@gmail.com>
2016-05-06 16:32:14 -0400

    Next and previous with same parity

--- src/main/java/com/ro0sterjam/ctci/Binary.java
+++ src/main/java/com/ro0sterjam/ctci/Binary.java
@@ -5,21 +5,102 @@ package com.ro0sterjam.ctci;
  */
 public class Binary {
 
-    public static int[] toBinaryArray(int value) {
-        int[] binary = new int[32];
-        int i = 32;
+    public static String toBinary(int value) {
+        if (value == 0) {
+            return "0";
+        }
+        StringBuilder binary = new StringBuilder();
         while (value > 0) {
-            binary[--i] = value % 2;
+            binary.append(value % 2);
             value = value >> 1;
         }
-        return binary;
+        return binary.reverse().toString();
+    }
+
+    public static String toBinary(double value) {
+        if (value == 0.0) {
+            return "0.0";
+        }
+        StringBuilder binary = new StringBuilder("0.");
+        double place = 0.5;
+        while (place > 0.0000000001 && value > 0) {
+            if (value >= place) {
+                binary.append(1);
+                value -= place;
+            } else {
+                binary.append(0);
+            }
+            place /= 2.0;
+        }
+        return binary.toString();
+    }
+
+    public static int fromBinary(String binary) {
+        int n = 0;
+        for (char c : binary.toCharArray()) {
+            n <<= 1;
+            if (c == '1') {
+                n += 1;
+            }
+        }
+        return n;
     }
 
     public static int replace(int target, int source, int start, int end) {
         int mask = (~0 << (start + 1)) | ((1 << end) - 1);
         target &= mask;
-        source = source << end;
+        source <<= end;
         return target | source;
+    }
+
+    public static int nextWithSameParity(int n) {
+        if (n == 0 || n == Integer.MAX_VALUE) {
+            return -1;
+        }
+        int offset = 0;
+        while (n % 2 == 0) {
+            offset++;
+            n /= 2;
+        }
+        int mask = 1;
+        while (n % 2 == 1) {
+            mask <<= 1;
+            offset++;
+            n /= 2;
+            if (n == 0) {
+                return -1;
+            }
+        }
+        n += 1;
+        n <<= offset;
+        mask = (mask >> 1) - 1;
+        return n | mask;
+    }
+
+    public static int prevWithSameParity(int n) {
+        if (n == 0 || n == Integer.MAX_VALUE) {
+            return -1;
+        }
+        int mask = 1;
+        int offset = 0;
+        while (n % 2 == 1) {
+            mask <<= 1;
+            offset++;
+            n /= 2;
+        }
+        mask = (mask << 1) - 1;
+        while (n % 2 == 0) {
+            mask <<= 1;
+            offset++;
+            n /= 2;
+            if (n == 0) {
+                return -1;
+            }
+        }
+        n -= 1;
+        n <<= offset;
+        mask = (mask >> 1);
+        return n | mask;
     }
 
 }
--- src/test/java/com/ro0sterjam/ctci/BinaryTest.java
+++ src/test/java/com/ro0sterjam/ctci/BinaryTest.java
@@ -2,9 +2,7 @@ package com.ro0sterjam.ctci;
 
 import org.junit.Test;
 
-import static com.ro0sterjam.ctci.Binary.replace;
-import static com.ro0sterjam.ctci.Binary.toBinaryArray;
-import static org.junit.Assert.assertArrayEquals;
+import static com.ro0sterjam.ctci.Binary.*;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,49 +11,155 @@ import static org.junit.Assert.assertEquals;
 public class BinaryTest {
 
     @Test
-    public void testToBinaryArray_zero() {
-        assertArrayEquals(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, toBinaryArray(0));
+    public void testToBinary_zero() {
+        assertEquals("0", toBinary(0));
     }
 
     @Test
-    public void testToBinaryArray_one() {
-        assertArrayEquals(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, toBinaryArray(1));
+    public void testToBinary_one() {
+        assertEquals("1", toBinary(1));
     }
 
     @Test
-    public void testToBinaryArray_two() {
-        assertArrayEquals(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, toBinaryArray(2));
+    public void testToBinary_two() {
+        assertEquals("10", toBinary(2));
     }
 
     @Test
-    public void testToBinaryArray_three() {
-        assertArrayEquals(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 }, toBinaryArray(3));
+    public void testToBinary_three() {
+        assertEquals("11", toBinary(3));
     }
 
     @Test
-    public void testToBinaryArray_876() {
-        assertArrayEquals(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0 }, toBinaryArray(876));
+    public void testToBinary_876() {
+        assertEquals("1101101100", toBinary(876));
     }
 
     @Test
-    public void testToBinaryArray_maxInt() {
-        assertArrayEquals(new int[]{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, toBinaryArray(Integer.MAX_VALUE));
+    public void testToBinary_maxInt() {
+        assertEquals("1111111111111111111111111111111", toBinary(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testFromBinary_zero() {
+        assertEquals(0, fromBinary("0"));
+    }
+
+    @Test
+    public void testFromBinary_one() {
+        assertEquals(1, fromBinary("1"));
+    }
+
+    @Test
+    public void testFromBinary_two() {
+        assertEquals(2, fromBinary("10"));
+    }
+
+    @Test
+    public void testFromBinary_three() {
+        assertEquals(3, fromBinary("11"));
+    }
+
+    @Test
+    public void testFromBinary_876() {
+        assertEquals(876, fromBinary("1101101100"));
+    }
+
+    @Test
+    public void testFromBinary_maxInt() {
+        assertEquals(Integer.MAX_VALUE, fromBinary("1111111111111111111111111111111"));
     }
 
     @Test
     public void testReplace_subsetSimple() {
-        assertEquals(1100, replace(1024, 19, 6, 2));
+        assertEquals(fromBinary("0000010001001100"), replace(fromBinary("0000010000000000"), fromBinary("10011"), 6, 2));
     }
 
     @Test
     public void testReplace_leftEdge() {
-        assertEquals(1807, replace(1871, 3, 6, 2));
+        assertEquals(fromBinary("0000011100001111"), replace(fromBinary("0000011101001111"), fromBinary("00011"), 6, 2));
     }
 
     @Test
     public void testReplace_rightEdge() {
-        assertEquals(3347, replace(3423, 4, 6, 2));
+        assertEquals(fromBinary("0000110100010011"), replace(fromBinary("0000110101011111"), fromBinary("00100"), 6, 2));
     }
+
+    @Test
+    public void testToBinary_zeroDouble() {
+        assertEquals("0.0", toBinary(0.0));
+    }
+
+    @Test
+    public void testToBinary_justShyOf1() {
+        assertEquals("0.111111111111111111111111111111111", toBinary(0.9999999999999999999));
+    }
+
+    @Test
+    public void testToBinary_simple() {
+        assertEquals("0.111", toBinary(0.875));
+    }
+
+    @Test
+    public void testToBinary_complex() {
+        assertEquals("0.101110000101000111101011100001010", toBinary(0.72));
+    }
+
+    @Test
+    public void testNextWithSameParity_zero() {
+        assertEquals(-1, nextWithSameParity(0));
+    }
+
+    @Test
+    public void testNextWithSameParity_maxInteger() {
+        assertEquals(-1, nextWithSameParity(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testNextWithSameParity_singleOneInFront() {
+        assertEquals(-1, nextWithSameParity(fromBinary("1000000000000000000000000000000")));
+    }
+
+    @Test
+    public void testNextWithSameParity_allOnesThenAllZeros() {
+        assertEquals(-1, nextWithSameParity(fromBinary("1111111111111110000000000000000")));
+    }
+
+    @Test
+    public void testNextWithSameParity_singleOne() {
+        assertEquals(fromBinary("11011001111000"), nextWithSameParity(fromBinary("11011001110100")));
+    }
+
+    @Test
+    public void testNextWithSameParity_multipleOnes() {
+        assertEquals(fromBinary("11011010001111"), nextWithSameParity(fromBinary("11011001111100")));
+    }
+
+    @Test
+    public void testPreviousWithSameParity_zero() {
+        assertEquals(-1, prevWithSameParity(0));
+    }
+
+    @Test
+    public void testPreviousWithSameParity_maxInteger() {
+        assertEquals(-1, prevWithSameParity(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testPreviousWithSameParity_allZerosThenAllOnes() {
+        assertEquals(-1, prevWithSameParity(fromBinary("0000000000000000111111111111111")));
+    }
+
+    @Test
+    public void testPreviousWithSameParity_singleZeroBetweenOnes() {
+        assertEquals(fromBinary("11011001101111"), prevWithSameParity(fromBinary("11011001110111")));
+    }
+
+    @Test
+    public void testPreviousWithSameParity_multipleZerosBetweenOnes() {
+        assertEquals(fromBinary("10011101110000"), prevWithSameParity(fromBinary("10011110000011")));
+    }
+
 
 
 }
